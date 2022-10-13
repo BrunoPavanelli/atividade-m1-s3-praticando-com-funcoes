fe4ea0880a1b15d14f38628fa4f9828d23b97c1c
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2022-10-13 17:35:06 -0300

    seguindo fazendo os exercicios

---
+++ script.js
@@ -0,0 +1,47 @@
+// //ex 1
+// alert("Ex 1:")
+// function lerValor() {
+//     let x = Number(prompt("Digite um valor"))
+//     return alert(`${x}`)
+// }
+// lerValor()
+
+//ex 2
+// alert("Ex 2:")
+// function soma3(x,y,z) {
+//     let soma = x + y+ z
+//     return alert(`${soma}`)
+// }
+
+// let x = Number(prompt("Digite um valor: "))
+// let y = Number(prompt("Digite um valor: "))
+// let z = Number(prompt("Digite um valor: "))
+
+// soma3(x,y,z)
+
+//ex 3
+// function mediaAnual (notaSemestre1, notaSemestre2) {
+//     let media = (notaSemestre1 + notaSemestre2) / 12
+//     return alert(`${media.toFixed(2)}`)
+// }
+
+// let notaSemestre1 = Number(prompt("Digita a nota do primeiro semestre"))
+// let notaSemestre2 = Number(prompt("Digite a nota do segundo semestre:"))
+
+// mediaAnual(notaSemestre1, notaSemestre2)
+
+//ex 4
+// alert("ex 4")
+// function verificadorDivisor(a, b) {
+//     let verificando = a % b
+//     if (verificando === 0) {
+//         return alert("Divsao exata")
+//     } else {
+//         return alert(`Divisao nao exata, e o resto e: ${verificando}`)
+//     }
+// }
+
+// let a = Number(prompt("Digite um valor"))
+// let b = Number(prompt("Digite um valor"))
+
+// verificadorDivisor(a,b)
